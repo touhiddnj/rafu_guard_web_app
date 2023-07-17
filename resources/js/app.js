cf5e95c0ad793a8eb5de031b9440eb033474552c
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-07-17 11:22:05 +0600

    Init

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import "bootstrap";
+
+import { createApp } from "vue";
+// import Welcome from "./components/Welcome";
+
+const app = createApp({});
+
+// app.component("welcome", Welcome);
+
+app.mount("#app");
