392d38e908f4acdbd3f8b4ed90221551deb31c60
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-07-19 13:23:52 +0600

    Update extension_index.blade.php

--- resources/views/extension/extension_index.blade.php
+++ resources/views/extension/extension_index.blade.php
@@ -10,6 +10,23 @@
 <!-- Basic Bootstrap Table -->
 <div class="card">
   <h5 class="card-header">Safeguarding Against Malicious Browser Extensions</h5>
+  <div class="card-body">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mb-4">
+          <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <div class="card-subtitle text-muted mb-3">Card subtitle</div>
+          <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="javascript:void(0)" class="card-link">Card link</a>
+          <a href="javascript:void(0)" class="card-link">Another link</a>
+          </div>
+          </div>
+      </div>
+    </div>
+  </div>
  
  
 </div>
