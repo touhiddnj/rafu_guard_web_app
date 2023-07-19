e401af6c2e6a1fc0890f03241be779e08db64a2a
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-07-19 19:08:12 +0600

    Update extension_index.blade.php

--- resources/views/extension/extension_index.blade.php
+++ resources/views/extension/extension_index.blade.php
@@ -394,10 +394,10 @@
 </h4>
 
 <!-- Basic Bootstrap Table -->
-<div class="card" id="extension-list">
+<div class="card">
   <h5 class="card-header">Safeguarding Against Malicious Browser Extensions</h5>
   <div class="card-body">
-    <div class="row">
+    <div class="row" id="extension-list">
       <div class="col-md-4">
         <div class="card mb-4">
           <div class="card-body">
