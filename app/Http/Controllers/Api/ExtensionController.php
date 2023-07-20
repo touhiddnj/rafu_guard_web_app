a2b0ce0d44d737ed298b0bcdd2e542281c7f4cef
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-07-20 12:51:05 +0600

    Update ExtensionController.php

--- app/Http/Controllers/Api/ExtensionController.php
+++ app/Http/Controllers/Api/ExtensionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Models\CustomUrlBlock;
 use Illuminate\Http\Request;
+
+use App\Models\CustomUrlBlock;
+use App\Http\Controllers\Controller;
+use Response;
 
 class ExtensionController extends Controller
 {
@@ -14,6 +16,8 @@ class ExtensionController extends Controller
 
         $list = CustomUrlBlock::where('user_id', $userId)->pluck('url')->toArray();
 
-        dd($list);
+        // dd($list);
+
+        return Response::json($list);
     }
 }
