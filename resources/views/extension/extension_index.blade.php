5cd8061f8bf7883f002401e67b6c4db04fb7adf8
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-08-03 00:06:19 +0600

    Menu Update

--- resources/views/extension/extension_index.blade.php
+++ resources/views/extension/extension_index.blade.php
@@ -397,23 +397,63 @@
 <div class="card">
   <h5 class="card-header">Safeguarding Against Malicious Browser Extensions</h5>
   <div class="card-body">
-    <div class="row" id="extension-list">
-{{--       <div class="col-md-4">
-        <div class="card mb-4">
-          <div class="card-body">
-          <h5 class="card-title ext-title">Volume Booster</h5>
-          <div class="card-subtitle text-muted mb-3 ext-short-title">Volume Booster</div>
-          <p class="card-text ext description">
-            Improve your writing with Grammarly's communicatio…heck, grammar check, punctuation check, and more..
+	<ul class="nav nav-tabs" role="tablist">
+        <li class="nav-item">
+          <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab" data-bs-target="#navs-top-home" aria-controls="navs-top-home" aria-selected="true">Home</button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#navs-top-profile" aria-controls="navs-top-profile" aria-selected="false">Profile</button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#navs-top-messages" aria-controls="navs-top-messages" aria-selected="false">Messages</button>
+        </li>
+      </ul>
+      <div class="tab-content">
+        <div class="tab-pane fade show active" id="navs-top-home" role="tabpanel">
+			<div class="row" id="extension-list">
+				{{--       <div class="col-md-4">
+						<div class="card mb-4">
+						  <div class="card-body">
+						  <h5 class="card-title ext-title">Volume Booster</h5>
+						  <div class="card-subtitle text-muted mb-3 ext-short-title">Volume Booster</div>
+						  <p class="card-text ext description">
+							Improve your writing with Grammarly's communicatio…heck, grammar check, punctuation check, and more..
+						  </p>
+						  <button type="button" class="btn btn-sm btn-secondary btn-toggle ext-enable" data-toggle="button" aria-pressed="false" autocomplete="off">
+							<div class="handle"></div>
+						  </button>
+						  <a href="javascript:void(0)" class="card-danger text-danger ext-remove">Remove</a>
+						  </div>
+						  </div>
+					  </div> --}}
+					</div>
+        </div>
+        <div class="tab-pane fade" id="navs-top-profile" role="tabpanel">
+          <p>
+            Donut dragée jelly pie halvah. Danish gingerbread bonbon cookie wafer candy oat cake ice cream. Gummies
+            halvah
+            tootsie roll muffin biscuit icing dessert gingerbread. Pastry ice cream cheesecake fruitcake.
           </p>
-          <button type="button" class="btn btn-sm btn-secondary btn-toggle ext-enable" data-toggle="button" aria-pressed="false" autocomplete="off">
-            <div class="handle"></div>
-          </button>
-          <a href="javascript:void(0)" class="card-danger text-danger ext-remove">Remove</a>
-          </div>
-          </div>
-      </div> --}}
+          <p class="mb-0">
+            Jelly-o jelly beans icing pastry cake cake lemon drops. Muffin muffin pie tiramisu halvah cotton candy
+            liquorice caramels.
+          </p>
+        </div>
+        <div class="tab-pane fade" id="navs-top-messages" role="tabpanel">
+          <p>
+            Oat cake chupa chups dragée donut toffee. Sweet cotton candy jelly beans macaroon gummies cupcake gummi
+            bears
+            cake chocolate.
+          </p>
+          <p class="mb-0">
+            Cake chocolate bar cotton candy apple pie tootsie roll ice cream apple pie brownie cake. Sweet roll icing
+            sesame snaps caramels danish toffee. Brownie biscuit dessert dessert. Pudding jelly jelly-o tart brownie
+            jelly.
+          </p>
+        </div>
+      </div>
     </div>
+
   </div>
  
  
