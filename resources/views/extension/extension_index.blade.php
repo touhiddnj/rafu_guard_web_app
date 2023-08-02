afe0c43c47fd2ae20409fd0d5774475537fb949b
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-08-03 00:27:20 +0600

    Update extension_index.blade.php

--- resources/views/extension/extension_index.blade.php
+++ resources/views/extension/extension_index.blade.php
@@ -429,15 +429,9 @@
 					</div>
         </div>
         <div class="tab-pane fade" id="navs-top-profile" role="tabpanel">
-          <p>
-            Donut dragée jelly pie halvah. Danish gingerbread bonbon cookie wafer candy oat cake ice cream. Gummies
-            halvah
-            tootsie roll muffin biscuit icing dessert gingerbread. Pastry ice cream cheesecake fruitcake.
-          </p>
-          <p class="mb-0">
-            Jelly-o jelly beans icing pastry cake cake lemon drops. Muffin muffin pie tiramisu halvah cotton candy
-            liquorice caramels.
-          </p>
+			
+			<h4 style="text-align: center">Premium</h4>
+
         </div>
         <div class="tab-pane fade" id="navs-top-messages" role="tabpanel">
           <p>
