ad2989b802e8ad45136e338000848e36082e1cf7
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-08-10 21:22:41 +0600

    Update footer.blade.php

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -28,7 +28,7 @@ if ($accessToken && !$accessToken->expired()) {
     </div>
     <div>
       <a href="{{ config('variables.licenseUrl') ? config('variables.licenseUrl') : '#' }}" class="footer-link me-4" target="_blank">License</a>
-      <a href="{{ config('variables.moreThemes') ? config('variables.moreThemes') : '#' }}" target="_blank" class="footer-link me-4">More Themes</a>
+      {{-- <a href="{{ config('variables.moreThemes') ? config('variables.moreThemes') : '#' }}" target="_blank" class="footer-link me-4">More Themes</a> --}}
       <a href="{{ config('variables.documentation') ? config('variables.documentation') : '#' }}" target="_blank" class="footer-link me-4">Documentation</a>
       <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
     </div>
