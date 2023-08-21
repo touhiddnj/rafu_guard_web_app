b12b92ba4bb73e6c8515318501f0f91bbfd8cd4e
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-08-21 22:49:54 +0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -350,7 +350,7 @@
         <div class="row">
           <div class="footer__bottom">
             <div class="footer__bottom-copy">
-              <h6>&copy; {{ date('y') }} Rafu Guard</h6>
+              <h6>&copy; {{ date('Y') }} Rafu Guard</h6>
             </div>
             <div class="footer__bottom-social">
               <h6>Follow us<span>-</span></h6>
