933b96566dc7e661b30cfd4d8c61b069effd6ba4
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-08-21 13:46:04 +0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Finance</title>
+  <title>Landing - Rafu Guard</title>
 
   <link rel="apple-touch-icon" sizes="180x180" href="/themes/lander/assets/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/themes/lander/assets/favicon/favicon-32x32.png">
