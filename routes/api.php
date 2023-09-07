14b89b21791559f50b5d138a0a6fb64885e90176
Mohammad Tohidul Islam <touhid@rafusoft.com>
2023-09-08 02:19:16 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->group(function () {
+Route::middleware(['auth:api', 'throttle:3000,1'])->group(function () {
     Route::get('/custom-blocking-list', [ExtensionController::class, 'customBlockingList']);
     Route::get('/custom-blocking-domain', [ExtensionController::class, 'customBlockingDomainList']);
     Route::post('/custom-blocking-add', [ExtensionController::class, 'customBlockingAdd']);
